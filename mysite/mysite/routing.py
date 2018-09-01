4fb7a9a725d104bcff3a9961ec0c51430f2c82a9
alexHooper <alexmohack@gmail.com>
2018-09-01 20:14:51 +0530

    create root routing configuration using chat routes

--- mysite/mysite/routing.py
+++ mysite/mysite/routing.py
@@ -1,5 +1,12 @@
-from channels.routing import ProtocolTypeRouter
+import chat.routing
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 application = ProtocolTypeRouter({
-	# (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    )
 })
