22006a982323d4db8a03534541bff6e7ba0ee86f
alexHooper <alexmohack@gmail.com>
2018-09-01 16:51:19 +0530

    create url for room_view

--- mysite/chat/urls.py
+++ mysite/chat/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, room_view
 
 urlpatterns = [
 	path('', index, name='index'),
+	path('<room_name>', room_view, name='room-view'),
 ]
