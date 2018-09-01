42b1af025f2a15b41029ee069f946f7dc7235df7
alexHooper <alexmohack@gmail.com>
2018-09-01 16:49:51 +0530

    add view for showing room

--- mysite/chat/views.py
+++ mysite/chat/views.py
@@ -1,4 +1,14 @@
+import json
+
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
+
 
 def index(request):
 	return render(request, 'chat/index.html', {})
+
+
+def room_view(request, room_name):
+	return render(request, 'chat/room.html', {
+		'room_name_json': mark_safe(json.dumps(room_name))
+	})
